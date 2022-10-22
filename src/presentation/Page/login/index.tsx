465df9d2840317174b9f453444d3452cfb1fccfd
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-22 12:30:38 -0300

    feat(login): deve conter elemento título

--- src/presentation/Page/login/index.tsx
+++ src/presentation/Page/login/index.tsx
@@ -1,9 +1,12 @@
 function Login() {
   return (
-    <>
+    <div className="container color-primary">
+      <header className="heading">
+        <h1>Bem vindo ao Gymr</h1>
+      </header>
       <button type="button">Entrar</button>
       <button type="button">Cadastrar</button>
-    </>
+    </div>
   );
 }
 export default Login;
