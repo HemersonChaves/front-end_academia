4d6290b638c7e653b2930bf92eabd59fe3bd01c1
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-22 12:28:20 -0300

    test(login): deve conter elemento título

--- src/presentation/Page/login/login.spec.tsx
+++ src/presentation/Page/login/login.spec.tsx
@@ -1,7 +1,16 @@
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import App from '../../../App';
 
 describe('Component Login', () => {
+  test('deve conter o elemento título', () => {
+    render(<App />);
+
+    const h1Element = screen.getByRole('heading', {
+      name: /bem vindo ao gymr/i,
+    });
+
+    expect(h1Element).toBeInTheDocument();
+  });
   test('deve iniciar com botões de cadastro ou de login ', () => {
     const { getByText } = render(<App />);
     // encontrar o botão iniciar sessão
@@ -13,4 +22,6 @@ describe('Component Login', () => {
     expect(botaoIniciarSessao).toBeInTheDocument();
     expect(botaoCadastrar).toBeInTheDocument();
   });
+  test.todo('Testar se o botão redireciona para o cadastro de novo usuário');
+  test.todo('Testar se o botão redireciona para area de login');
 });
