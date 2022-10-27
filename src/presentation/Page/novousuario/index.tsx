618eefcb677463687d586194173092d2936ef61b
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-26 21:37:23 -0300

    feat(novo): adiciona component novo usuário

--- src/presentation/Page/novousuario/index.tsx
+++ src/presentation/Page/novousuario/index.tsx
@@ -1,7 +1,40 @@
+import style from './styles.module.css';
+
 function NovoUsuario() {
   return (
-    <div data-testid="novo-usuario">
-      <h1>Novo usuário</h1>
+
+    <div className={style.container} data-testid="novo-usuario">
+      <header>
+        <nav className={style.navigation}>
+          <a href="/">
+            <i className="las la-lg la-angle-left" />
+          </a>
+        </nav>
+        <div className={style.heading}>
+          <h1>Criar nova</h1>
+        </div>
+      </header>
+      <section>
+        <div className={style.form_group}>
+          <input id="nome" type="text" className={style.form_control} required />
+          <label htmlFor="nome">Usuário</label>
+        </div>
+        <div className={style.form_group}>
+          <input id="email" type="email" className={style.form_control} required />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div className={style.form_group}>
+          <input id="senha" type="password" className={style.form_control} required />
+          <label htmlFor="senha">Senha</label>
+        </div>
+        <div className={style.form_group}>
+          <input id="confirmacao-senha" type="password" className={style.form_control} required />
+          <label htmlFor="confirmacao-senha">Confirmação de senha</label>
+        </div>
+        <div className={style.form_button}>
+          <button type="submit" className={style.btn}>Cadastrar</button>
+        </div>
+      </section>
     </div>
 
   );
