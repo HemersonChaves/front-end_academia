39a5016a1ef995c5462ca1e080af26843cd92ea4
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-11-04 23:10:55 -0300

    test(login): deve redireciona para o cadastro de novo usuário

--- src/presentation/Page/login/login.spec.tsx
+++ src/presentation/Page/login/login.spec.tsx
@@ -1,17 +1,22 @@
 import { screen, render, waitFor } from '@testing-library/react';
-import {
-  MemoryRouter,
-  useLocation,
-} from 'react-router-dom';
+import * as ReactRouterDom from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import RoutesPages from '../../../router/router';
 
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual<typeof ReactRouterDom>('react-router-dom'),
+  useNavigate: () => mockedNavigate,
+}));
+
 describe('Component Login', () => {
+  const interacaoUsuario = userEvent.setup();
   test('deve conter o elemento título', () => {
     render(
-      <MemoryRouter>
+      <ReactRouterDom.BrowserRouter>
         <RoutesPages />
-      </MemoryRouter>,
+      </ReactRouterDom.BrowserRouter>,
     );
 
     const h1Element = screen.getByRole('heading', {
@@ -22,9 +27,9 @@ describe('Component Login', () => {
   });
   test('deve iniciar com botões de cadastro ou de login e lembrar senha ', () => {
     render(
-      <MemoryRouter>
+      <ReactRouterDom.BrowserRouter>
         <RoutesPages />
-      </MemoryRouter>,
+      </ReactRouterDom.BrowserRouter>,
     );
     // encontrar o botão iniciar sessão
     const botaoIniciarSessao = screen.getByText('Entrar');
@@ -42,36 +47,33 @@ describe('Component Login', () => {
     expect(botaoCadastrar).toBeInTheDocument();
     expect(botaoLembrarSenha).toBeInTheDocument();
   });
-  test('deve redireciona para o cadastro de novo usuário', async () => {
-    render(
-      <MemoryRouter>
-        <RoutesPages />
-      </MemoryRouter>,
-    );
-    const handlePageCadastro = jest.spyOn(console, 'log');
-    // encontrar o botão iniciar sessão
-    const botaoCadastrar = screen.getByText('Cadastrar');
-
-    await userEvent.click(botaoCadastrar);
-    // toMatchSnapshot()
-    await waitFor(() => {
-      expect(handlePageCadastro).toHaveBeenCalled();
-      // expect(botaoCadastrar).toHaveAttribute('href', '/cadastro');
-    });
-  });
   test('deve redirecionar para página de erro 404', async () => {
     const badRoute = '/some/bad/route';
 
     // use <MemoryRouter> when you want to manually control the history
     render(
-      <MemoryRouter initialEntries={[badRoute]}>
+      <ReactRouterDom.MemoryRouter initialEntries={[badRoute]}>
         <RoutesPages />
-      </MemoryRouter>,
+      </ReactRouterDom.MemoryRouter>,
     );
 
     // verify navigation to "no match" route
     await waitFor(() => {
       expect(screen.getByText(/erro 404/i)).toBeInTheDocument();
+    });
+  });
+  test('deve redireciona para o cadastro de novo usuário', async () => {
+    render(
+      <ReactRouterDom.BrowserRouter>
+        <RoutesPages />
+      </ReactRouterDom.BrowserRouter>,
+    );
+    // encontrar o botão iniciar sessão
+    const botaoCadastrar = screen.getByText('Cadastrar');
+
+    await interacaoUsuario.click(botaoCadastrar);
+    await waitFor(() => {
+      expect(mockedNavigate).toHaveBeenNthCalledWith(1, 'novousuario');
     });
   });
   test.todo('Testar se o botão redireciona para area de login');
