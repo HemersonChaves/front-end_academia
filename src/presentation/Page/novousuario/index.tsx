3e813273477cb6002cb448bc9b2a057b3e6b16bb
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-11-14 08:46:37 -0300

    refactor(novousuario): deve iniciar com inputs de nome

--- src/presentation/Page/novousuario/index.tsx
+++ src/presentation/Page/novousuario/index.tsx
@@ -12,19 +12,19 @@ function NovoUsuario() {
       </header>
       <section>
         <div className={style.form_group}>
-          <input id="nome" type="text" className={style.form_control} required />
-          <label htmlFor="nome">Usuário</label>
+          <input id="nome" name="nome" type="text" className={style.form_control} required />
+          <label htmlFor="nome">Nome</label>
         </div>
         <div className={style.form_group}>
-          <input id="email" type="email" className={style.form_control} required />
+          <input id="email" name="email" type="email" className={style.form_control} required />
           <label htmlFor="email">Email</label>
         </div>
         <div className={style.form_group}>
-          <input id="senha" type="password" className={style.form_control} required />
+          <input id="senha" name="senha" type="password" className={style.form_control} required />
           <label htmlFor="senha">Senha</label>
         </div>
         <div className={style.form_group}>
-          <input id="confirmacao-senha" type="password" className={style.form_control} required />
+          <input id="confirmacao-senha" name="confirmarsenha" type="password" className={style.form_control} required />
           <label htmlFor="confirmacao-senha">Confirmação de senha</label>
         </div>
         <div className={style.form_button}>
