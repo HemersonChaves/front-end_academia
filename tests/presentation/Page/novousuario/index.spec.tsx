1c485712ea6d2520c50db7f37087e491f169c842
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-11-14 18:47:45 -0300

    test(novousuario): deve iniciar com inputs de nome vazios

--- tests/presentation/Page/novousuario/index.spec.tsx
+++ tests/presentation/Page/novousuario/index.spec.tsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 import NovoUsuario from '../../../../src/presentation/Page/novousuario';
 
 describe('Componente <novousuario />', () => {
-  test('deve iniciar com inputs de nome', () => {
+  test('deve iniciar com inputs de nome vazios', () => {
     render(<NovoUsuario />);
 
     const inputNome = screen.getByRole('textbox', { name: /Nome/i });
-
+    expect(inputNome).toHaveValue('');
     expect(inputNome).toBeInTheDocument();
   });
 });
