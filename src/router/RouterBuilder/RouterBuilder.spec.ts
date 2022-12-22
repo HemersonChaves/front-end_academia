c0bb6f85e090a3ddb4ffb684cad953cf5eddaa9f
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-12-22 20:34:50 -0300

    test(RouterBuilder): deve ser possível adicionar um path

--- src/router/RouterBuilder/RouterBuilder.spec.ts
+++ src/router/RouterBuilder/RouterBuilder.spec.ts
@@ -1,12 +1,18 @@
 import RouterBuilder from '.';
+import RootPath from '../PathPage/RootPath';
 
 describe('RouterBuilder', () => {
-  let instance: RouterBuilder;
+  const instance: RouterBuilder = new RouterBuilder();
+  const spyOnRouterBuilder = jest.spyOn(instance, 'addPath');
 
   beforeEach(() => {
-    instance = new RouterBuilder();
+    jest.clearAllMocks();
   });
   test('deve instanciar uma classe RouterBuilder', () => {
     expect(instance).toBeInstanceOf(RouterBuilder);
+  });
+  test('deve ser possível adicionar um path ', () => {
+    instance.addPath(RootPath.MAIN);
+    expect(spyOnRouterBuilder).toBeCalledTimes(1);
   });
 });
