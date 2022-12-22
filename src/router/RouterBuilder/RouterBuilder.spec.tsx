cabf0c6e4422ceead1a4ffd284650c606d048aaa
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-12-22 20:48:06 -0300

    test(RouterBuilder): deve ser possível adicionar um Element

--- src/router/RouterBuilder/RouterBuilder.spec.tsx
+++ src/router/RouterBuilder/RouterBuilder.spec.tsx
@@ -3,7 +3,6 @@ import RootPath from '../PathPage/RootPath';
 
 describe('RouterBuilder', () => {
   const instance: RouterBuilder = new RouterBuilder();
-  const spyOnRouterBuilder = jest.spyOn(instance, 'addPath');
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -11,8 +10,14 @@ describe('RouterBuilder', () => {
   test('deve instanciar uma classe RouterBuilder', () => {
     expect(instance).toBeInstanceOf(RouterBuilder);
   });
-  test('deve ser possível adicionar um path ', () => {
+  test('deve ser possível adicionar um path', () => {
+    const spyOnRouterBuilder = jest.spyOn(instance, 'addPath');
     instance.addPath(RootPath.MAIN);
+    expect(spyOnRouterBuilder).toBeCalledTimes(1);
+  });
+  test('deve ser possível adicionar um Element', () => {
+    const spyOnRouterBuilder = jest.spyOn(instance, 'addElement');
+    instance.addElement(<span>elemento react</span>);
     expect(spyOnRouterBuilder).toBeCalledTimes(1);
   });
 });
