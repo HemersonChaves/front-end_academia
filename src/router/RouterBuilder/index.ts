b19311fe066a8bdbcd9735f1884d70f6fec66b4f
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-12-22 18:40:39 -0300

    refactor(router): retirar array de addchildren e implement clear

--- src/router/RouterBuilder/index.ts
+++ src/router/RouterBuilder/index.ts
@@ -1,27 +1,35 @@
 import { ReactNode } from 'react';
 import { RouteObject } from 'react-router-dom';
-import RoutesPath from '../enum';
+import RoutesPath from '../PathPage/RootPath';
 
 interface IRouteBuilder {
   addPath(path: RoutesPath): void;
   addElement(element: ReactNode): void;
   addErrorElement(errorElement: ReactNode): void;
-  addChildren(children: RouteObject[]): void;
+  addChildren(children: RouteObject): void;
   build(): RouteObject;
 }
 class RouterBuilder implements IRouteBuilder {
   private route: RouteObject;
 
+  private routeChildren: RouteObject[];
+
   constructor() {
-    this.route = {} as RouteObject;
+    this.route = {};
+    this.routeChildren = [];
   }
 
   addErrorElement(errorElement: ReactNode) {
     this.route.errorElement = errorElement;
   }
 
-  addChildren(children: RouteObject[]) {
-    this.route.children = children;
+  addChildren(child: RouteObject) {
+    if (!this.routeChildren) {
+      this.routeChildren = [];
+    }
+    this.routeChildren.push(child);
+    delete this.route.children;
+    this.route.children = this.routeChildren;
   }
 
   addPath(path: RoutesPath) {
@@ -33,9 +41,23 @@ class RouterBuilder implements IRouteBuilder {
   }
 
   build(): RouteObject {
-    const routebulder = this.route;
-    this.route = {} as RouteObject;
-    return routebulder;
+    const routebuilt = { ...this.route };
+    this.clear();
+    return routebuilt;
+  }
+
+  private clear() {
+    delete this.route.path;
+    delete this.route.id;
+    delete this.route.loader;
+    delete this.route.action;
+    delete this.route.hasErrorBoundary;
+    delete this.route.shouldRevalidate;
+    delete this.route.handle;
+    delete this.route.index;
+    delete this.route.children;
+    delete this.route.element;
+    delete this.route.errorElement;
   }
 }
 
