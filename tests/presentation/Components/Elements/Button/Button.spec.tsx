037f865d3baf28ec36988c9f291ec08401dc793f
Hemerson Chaves <hemersonchaves20@hotmail.com>
2023-01-27 10:59:23 -0300

    test(Button): deve disparar um evento de click

--- tests/presentation/Components/Elements/Button/Button.spec.tsx
+++ tests/presentation/Components/Elements/Button/Button.spec.tsx
@@ -1,11 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Button from '../../../../../src/presentation/Components/Elements/Button';
 
 describe('Componente <Button />', () => {
+  const interacaoUsuario = userEvent.setup();
+
   test('deve iniciar com botão', () => {
     render(<Button>Teste</Button>);
 
-    const inputNome = screen.getByRole('button', { name: /Test/i });
-    expect(inputNome).toBeInTheDocument();
+    const button = screen.getByRole('button', { name: /Test/i });
+    expect(button).toBeInTheDocument();
+  });
+  test('deve disparar um evento de click', async () => {
+    const handleClicked = jest.fn();
+    // const handleClicked = () => { console.log('hhh'); };
+    render(<Button handleClick={handleClicked}>Teste</Button>);
+    // encontrar o botão iniciar sessão
+    const button = screen.getByRole('button', { name: /Test/i });
+
+    await interacaoUsuario.click(button);
+    expect(handleClicked).toHaveBeenCalledTimes(1);
   });
 });
